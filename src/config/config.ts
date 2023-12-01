55303073b7c2624b477eb85085280a24b0233f4e
Luca Restagno <luca.restagno@gmail.com>
2023-12-01 08:49:53 +0100

    add comments to config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,15 +2,27 @@ export const brandName = "My App";
 export const landingPageTitle = "My App";
 export const landingPageDescription = "Make money today with My App";
 export const websiteUrl = "https://myapp.com";
-export const demoCalendlyLink = "https://calendly.com/myself/15min";
 export const supportEmail = "support@email.com";
+
+// the users will be redirected to this page after sign in
+export const signInCallbackUrl = "/dashboard";
+
+// only needed if you have the "talk to us" button in the landing page
+export const demoCalendlyLink = "https://calendly.com/myself/15min";
+
+// used by MailChimp
 export const emailFrom = "no-reply@email.com";
+
+// social links
 export const discordLink = "https://discordlink";
 export const twitterLink = "https://x.com/johndoe";
 export const youTubeLink = "https://youtube.com/johndoe";
-export const signInCallbackUrl = "/dashboard";
+
 export const affiliateProgramLink =
   "https://yourstore.lemonsqueezy.com/affiliates";
 
+/* 
+  do not edit this
+*/
 export { pricingPlans } from "./pricing.constants";
 export { lifetimeDeals } from "./lifetime.constants";
