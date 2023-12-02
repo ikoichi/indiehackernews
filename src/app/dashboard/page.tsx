b54ced615d761591f4191baa5e4c63e6e7381eeb
Luca Restagno <luca.restagno@gmail.com>
2023-12-02 15:53:23 +0100

    improve private page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,40 @@
 "use client";
 
-import { Button, Center, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useSession } from "next-auth/react";
+import { Button, Center, Flex, Spinner, Stack, Text } from "@chakra-ui/react";
+import { signOut, useSession } from "next-auth/react";
+import { useState } from "react";
+import { TbLogout2 } from "react-icons/tb";
 
 const Dashboard = () => {
   const { data: session, status } = useSession();
+
+  const [isSigningOut, setSigningOut] = useState(false);
+  const onSignOut = () => {
+    setSigningOut(true);
+    signOut({
+      callbackUrl: "/",
+    });
+  };
 
   return (
     <Center minH="100vh">
       {status === "loading" && <Spinner color="brand.500" />}
       {status === "authenticated" && (
-        <p>You are logged in as {session?.user?.email}</p>
+        <Stack direction="column" spacing="16px">
+          <Flex className="text-slate-500 text-sm">
+            You are logged in as {session?.user?.email}
+          </Flex>
+          <Button
+            leftIcon={<TbLogout2 />}
+            onClick={onSignOut}
+            variant="outline"
+            size="sm"
+            isLoading={isSigningOut}
+            className="rounded-lg text-slate-600"
+          >
+            Log out
+          </Button>
+        </Stack>
       )}
       {status === "unauthenticated" && (
         <Stack>
