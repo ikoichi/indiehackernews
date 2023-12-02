d1730a742442cccf126a7a67e908d0daa3888276
Luca Restagno <luca.restagno@gmail.com>
2023-12-02 15:22:27 +0100

    add lemon squeezy affiliate store javascript snippet

--- src/components/LemonSqueezyAffiliateScript/LemonSqueezyAffiliateScript.tsx
+++ src/components/LemonSqueezyAffiliateScript/LemonSqueezyAffiliateScript.tsx
@@ -1,9 +1,19 @@
 import Script from "next/script";
+import { useEffect } from "react";
 
-export const LemonSqueezyAffiliateScript = () => (
-  <Script
-    id="lemon-squeezy-js"
-    defer
-    src="https://lmsqueezy.com/affiliate.js"
-  />
-);
+export const LemonSqueezyAffiliateScript = () => {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // @ts-ignore
+      window.lemonSqueezyAffiliateConfig = { store: "<store_name>" };
+    }
+  }, [typeof window !== "undefined"]);
+
+  return (
+    <Script
+      id="lemon-squeezy-js"
+      defer
+      src="https://lmsqueezy.com/affiliate.js"
+    />
+  );
+};
