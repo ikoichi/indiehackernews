357bdaf4814da6651675fd164bd41d11214c9311
Luca Restagno <luca.restagno@gmail.com>
2023-12-14 15:35:01 +0100

    fix lifetime component responsiveness

--- src/components/Lifetime/Lifetime.tsx
+++ src/components/Lifetime/Lifetime.tsx
@@ -17,17 +17,23 @@ export const Lifetime = () => {
         <Heading as="h4" fontSize="16px" color="brand.400" mb="16px">
           Lifetime deal
         </Heading>
-        <Heading as="h2" fontSize="48px" mb="32px" fontWeight="extrabold">
+        <Heading
+          as="h2"
+          fontSize={["26px", "40px", "48px"]}
+          lineHeight={["32px", "48px", "56px"]}
+          mb="32px"
+          fontWeight="extrabold"
+        >
           Start making money today.
         </Heading>
-        <Text>
+        <Text px="16px">
           Get access to all these features, forever, with our pre-sale lifetime
           deal.
         </Text>
-        <Text mt="8px">
+        <Text px="16px" mt="8px">
           We&apos;ll give you access as soon as the product is ready.
         </Text>
-        <Text fontSize="14px" color="blackAlpha.600" mt="24px">
+        <Text px="16px" fontSize="14px" color="blackAlpha.600" mt="24px">
           30-day refund if you&apos;re not satisfied.
         </Text>
       </Section>
