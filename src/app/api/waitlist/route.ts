b64f6011d5e51cc0391764d0a5147b1791b84813
Luca Restagno <luca.restagno@gmail.com>
2024-01-25 11:45:27 +0100

    improve waitlist route

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,31 +1,43 @@
+import { HttpStatusCode } from "axios";
+import { NextResponse } from "next/server";
 // import { createLoopsContact } from "@/libs/loops";
 // import { addMailChimpListMember } from "@/libs/mailchimp";
-import type { NextApiRequest, NextApiResponse } from "next";
-
-type ResponseData = {
-  result: boolean;
-};
 
 export async function POST(req: Request) {
   const body = await req.json();
   const email = body.email;
+  if (!email) {
+    return NextResponse.json(
+      { error: "Email is required" },
+      { status: HttpStatusCode.BadRequest }
+    );
+  }
+
   if (email) {
-    /* 
-    addMailChimpListMember({
-      email,
-      firstName: "",
-      lastName: "",
-      tags: ["waitlits"],
-    }); 
-    */
-    /* 
-    createLoopsContact({
-      email,
-      firstName: "",
-      lastName: "",
-      userGroup: "Waitlist",
-    }); 
-    */
+    try {
+      /* 
+      await addMailChimpListMember({
+        email,
+        firstName: "",
+        lastName: "",
+        tags: ["waitlits"],
+      }); 
+      */
+      /* 
+     await createLoopsContact({
+       email,
+       firstName: "",
+       lastName: "",
+       userGroup: "Waitlist",
+      }); 
+      */
+    } catch (err) {
+      console.error("Error while adding email contact", err);
+      return NextResponse.json(
+        { error: "An error occurred" },
+        { status: HttpStatusCode.InternalServerError }
+      );
+    }
   }
 
   return Response.json({ result: true });
