3763fcd7a182e3958efff596b7d5cba276c670ad
Luca Restagno <luca.restagno@gmail.com>
2024-03-10 16:53:19 +0100

    fix ranking

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -58,7 +58,7 @@ export async function GET(
   const resourcesRows: ResourceResponse[] =
     await prismaClient.$queryRaw(Prisma.sql`
     with rank as (
-      select id, (upvotes - 1) / (pow( (EXTRACT(EPOCH FROM (NOW() - "createdAt")) / 3600) + 2, 1.8)) as rank
+      select id, (upvotes) / (pow( (EXTRACT(EPOCH FROM (NOW() - "createdAt")) / 3600) + 2, 1.8)) as rank
       from "public"."Resource"
       group by id
     )
